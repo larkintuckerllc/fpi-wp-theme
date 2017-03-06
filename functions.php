4aaef20e3efe933168faa7d61b076f54b26f60f6
John Tucker <john@larkintuckerllc.com>
2017-03-06 10:57:34 -0500

    Restarting development.

--- functions.php
+++ functions.php
@@ -1,17 +1,167 @@
 <?php
-  function create_post_type() {
+  function fpi_create_post_type() {
+    $labels = array(
+     'name' => __( 'Indicators', 'fpi' ),
+     'singular_name' => __( 'Indicator', 'fpi' ),
+     'add_new' => __( 'Add New' , 'fpi' ),
+     'add_new_item' => __( 'Add New Indicator' , 'fpi' ),
+     'edit_item' =>  __( 'Edit Indicator' , 'fpi' ),
+     'new_item' => __( 'New Indicator' , 'fpi' ),
+     'view_item' => __('View Indicator', 'fpi'),
+     'search_items' => __('Search Indicators', 'fpi'),
+     'not_found' =>  __('No Indicators found', 'fpi'),
+     'not_found_in_trash' => __('No Indicators found in Trash', 'fpi'),
+    );
     register_post_type( 'fpi_indicator',
       array(
-        'labels' => array(
-          'name' => __( 'Indicators' ),
-          'singular_name' => __( 'Indicator' )
-        ),
+        'labels' => $labels,
         'public' => true,
         'has_archive' => false,
+        'menu_icon' => 'dashicons-list-view',
       )
     );
+    if( function_exists( 'register_field_group' ) ) {
+    	register_field_group(array (
+    		'id' => 'acf_fpi-indicator',
+    		'key' => 'acf_fpi-indicator',
+    		'title' => 'FPI Indicator',
+    		'fields' => array (
+    			array (
+    				'key' => 'field_58bd7f2ed0e8b',
+    				'label' => 'Name',
+    				'name' => 'name',
+    				'type' => 'text',
+    				'instructions' => 'User-friendly name of the fishery.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'formatting' => 'none',
+    				'maxlength' => '',
+    			),
+    			array (
+    				'key' => 'field_58bd7dbef7699',
+    				'label' => 'Image',
+    				'name' => 'image',
+    				'type' => 'image',
+    				'instructions' => '300px by 200px image representing fishery.',
+    				'required' => 1,
+    				'save_format' => 'url',
+    				'preview_size' => 'thumbnail',
+    				'library' => 'all',
+    			),
+    			array (
+    				'key' => 'field_58bd7df2f769a',
+    				'label' => 'Latitude',
+    				'name' => 'latitude',
+    				'type' => 'number',
+    				'instructions' => 'Latitude (+/- 90) representing fishery.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'min' => '-90',
+    				'max' => 90,
+    				'step' => '',
+    			),
+    			array (
+    				'key' => 'field_58bd7e14f769b',
+    				'label' => 'Longitude',
+    				'name' => 'longitude',
+    				'type' => 'number',
+    				'instructions' => 'Longitude (+/- 180) representing fishery.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'min' => '-180',
+    				'max' => 180,
+    				'step' => '',
+    			),
+    			array (
+    				'key' => 'field_58bd7e4af769c',
+    				'label' => 'Ecological',
+    				'name' => 'ecological',
+    				'type' => 'number',
+    				'instructions' => 'Ecological indicator (0-5) of the fishery.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'min' => 0,
+    				'max' => 5,
+    				'step' => '',
+    			),
+    			array (
+    				'key' => 'field_58bd7e6ff769d',
+    				'label' => 'Economic',
+    				'name' => 'economic',
+    				'type' => 'number',
+    				'instructions' => 'Economic indicator (0-5) of the fishery.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'min' => 0,
+    				'max' => 5,
+    				'step' => '',
+    			),
+    			array (
+    				'key' => 'field_58bd7e87f769e',
+    				'label' => 'Community',
+    				'name' => 'community',
+    				'type' => 'number',
+    				'instructions' => 'Community indicator (0-5) of the fishery.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'min' => 0,
+    				'max' => 5,
+    				'step' => '',
+    			),
+    		),
+    		'location' => array (
+    			array (
+    				array (
+    					'param' => 'post_type',
+    					'operator' => '==',
+    					'value' => 'fpi_indicator',
+    					'order_no' => 0,
+    					'group_no' => 0,
+    				),
+    			),
+    		),
+    		'options' => array (
+    			'position' => 'normal',
+    			'layout' => 'no_box',
+    			'hide_on_screen' => array (
+    				0 => 'permalink',
+    				1 => 'the_content',
+    				2 => 'excerpt',
+    				3 => 'discussion',
+    				4 => 'comments',
+    				5 => 'revisions',
+    				6 => 'slug',
+    				7 => 'author',
+    				8 => 'format',
+    				9 => 'featured_image',
+    				10 => 'categories',
+    				11 => 'tags',
+    				12 => 'send-trackbacks',
+    			),
+    		),
+    		'menu_order' => 0,
+    	));
+    }
   }
-  function add_theme_scripts() {
+  function fpi_add_theme_scripts() {
     wp_deregister_script( 'jquery' );
     wp_register_style('bootstrap',
       get_template_directory_uri() .
@@ -32,11 +182,10 @@
     wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap');
   }
-  function register_my_menus() {
+  function fpi_register_my_menus() {
     register_nav_menu('header-menu',__( 'Header Menu' ));
     register_nav_menu('tool-menu',__( 'Tool Menu' ));
   }
-  add_action('wp_enqueue_scripts', 'add_theme_scripts');
-  add_action( 'init', 'create_post_type' );
-  add_action( 'init', 'register_my_menus' );
-// REMOVED CLOSING TAG PER WP CODING STANDARDS
+  add_action('wp_enqueue_scripts', 'fpi_add_theme_scripts');
+  add_action( 'init', 'fpi_create_post_type' );
+  add_action( 'init', 'fpi_register_my_menus' );
