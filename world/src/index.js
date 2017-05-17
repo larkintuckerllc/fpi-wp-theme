c43864897040a5143683ac02de30d71272befb13
John Tucker <john@larkintuckerllc.com>
2017-05-17 16:52:48 -0400

    Added flow

--- world/src/index.js
+++ world/src/index.js
@@ -1,3 +1,5 @@
+// @flow
+import 'babel-polyfill';
 import './index.css';
 
 const cute = require('./cute.jpg');
@@ -5,4 +7,14 @@ const cute = require('./cute.jpg');
 const rootEl = document.getElementById('root');
 const imageEl = document.createElement('img');
 imageEl.src = cute;
+// $FlowFixMe
 rootEl.appendChild(imageEl);
+const stuff = {
+  a: 'apple',
+  b: 'banana',
+};
+const newStuff = {
+  ...stuff,
+  c: 'change',
+};
+window.console.log(newStuff);
