036a55c611fb8cf93f86cb9da4754deb6b4f16a4
John Tucker <john@larkintuckerllc.com>
2017-05-17 05:50:34 -0400

    Stub out world

---
+++ page-world-embed.php
@@ -0,0 +1,40 @@
+<?php
+  function fpi_world_add_scripts() {
+    wp_register_script('fpi_world_main',
+      get_template_directory_uri() .
+      '/world/dist/main.bundle.js',
+      array(), '2017051701', true);
+    wp_enqueue_script('fpi_world_main');
+  }
+  add_action('wp_enqueue_scripts', 'fpi_world_add_scripts');
+?>
+<?php
+  $indicators = array();
+  $args = array(
+    'post_type' => 'fpi_indicator',
+    'posts_per_page' => -1,
+  );
+  $loop = new WP_Query( $args );
+  while ( $loop->have_posts() ) : $loop->the_post();
+    $indicators[] = array(
+      'id' => get_the_ID(),
+      'link' => get_permalink(),
+      'name' => get_field('name'),
+      'image' => get_field('image'),
+      'latitude' => get_field('latitude'),
+      'longitude' => get_field('longitude'),
+      'ecological' => get_field('ecological'),
+      'economic' => get_field('economic'),
+      'community' => get_field('community')
+    );
+  endwhile;
+  wp_reset_query();
+?>
+<?php get_header(); ?>
+<div id="root"></div>
+<script>
+  window.indicators = <?php echo json_encode( $indicators ); ?>;
+  window.baseUrl = '<?php echo get_template_directory_uri(); ?>/world/dist/';
+  window.fpiEmbed = true;
+</script>
+<?php get_footer(); ?>
