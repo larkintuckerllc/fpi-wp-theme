909c6f28fac71b8db7235287341acfe27510e980
John Tucker <john@larkintuckerllc.com>
2017-05-18 13:11:00 -0400

    More

--- world/src/index.js
+++ world/src/index.js
@@ -1,12 +1,67 @@
 // @flow
 import 'babel-polyfill';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { scaleWidth, colorScale } from './util';
 import './index.css';
 
-const cute = require('./cute.jpg');
-
-const rootEl = document.getElementById('root');
-const imageEl = document.createElement('img');
-imageEl.src = cute;
-// $FlowFixMe
-rootEl.appendChild(imageEl);
-// const test = new Promise.resolve();
+const map = L.map('root').setView([0, 0], 0);
+L.tileLayer(
+  'http://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+  {
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ, TomTom, Intermap, iPC, USGS, FAO, NPS, NRCAN, GeoBase, Kadaster NL, Ordnance Survey, Esri Japan, METI, Esri China (Hong Kong), and the GIS User Community',
+  },
+).addTo(map);
+for (let i = 0; i < window.indicators.length; i += 1) {
+  const indicator = window.indicators[i];
+  const latLng = L.latLng(Number(indicator.latitude), Number(indicator.longitude));
+  const icon = L.icon({
+    iconUrl: indicator.image,
+    iconSize: [30, 20],
+    iconAnchor: [15, 10],
+  });
+  const marker = L.marker(
+    L.latLng(latLng),
+    {
+      icon,
+    },
+  );
+  const html = `
+    <div
+      class="root__popup__hero"
+      style="background-image: url(${indicator.image})"
+    >
+      <div
+        class="root__popup__hero__name"
+      >
+        ${indicator.name}
+      </div>
+    </div>
+    <div class="root__popup__indicator">
+      <div class="root__popup__indicator__name">
+        Ecological Performance
+      </div>
+      <div class="root__popup__indicator__value">
+        <div
+          class="root__popup__indicator__value__container"
+          style="width: ${scaleWidth(Number(indicator.ecological))}%"
+        >
+          <div
+            class="root__popup__indicator__value__container__bar"
+            style="background-color: ${colorScale(Number(indicator.ecological))}"
+          >
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="root__popup__scale">
+      <div class="root__popup__scale__good">Good</div>
+      <div>Poor</div>
+    </div>
+  `;
+  const popup = L.popup()
+    .setLatLng(latLng)
+    .setContent(html);
+  marker.bindPopup(popup);
+  marker.addTo(map);
+}
