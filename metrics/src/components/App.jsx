a1f508359ca158f896d532cd187139cab2b08b78
John Tucker <john@larkintuckerllc.com>
2017-07-29 15:44:17 -0400

    recompose

--- metrics/src/components/App.jsx
+++ metrics/src/components/App.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { PropTypes } from 'prop-types';
+import { compose, pure, setDisplayName, setPropTypes } from 'recompose';
 import * as fromDemo from '../ducks/demo';
 
-const App = ({ demo, setDemo }) => (
+const enhance = compose(
+  connect(
+    (state) => ({
+      demo: fromDemo.getDemo(state),
+    }),
+    {
+      setDemo: fromDemo.setDemo,
+    }
+  ),
+  pure,
+  setPropTypes({
+    demo: PropTypes.bool.isRequired,
+    setDemo: PropTypes.func.isRequired,
+  }),
+  setDisplayName('App')
+);
+export default enhance(({ demo, setDemo }) => (
   <div>
     <div style={{ color: demo ? 'red' : 'black' }}>Hello App</div>
     <button onClick={() => setDemo(!demo)} >Toggle</button>
   </div>
-);
-App.propTypes = {
-  demo: PropTypes.bool.isRequired,
-  setDemo: PropTypes.func.isRequired,
-};
-export default connect(
-  (state) => ({
-    demo: fromDemo.getDemo(state),
-  }),
-  {
-    setDemo: fromDemo.setDemo,
-  }
-)(App);
+));
