ddd0d61c6522588ae0d6089d2b855d1061d2359c
John Tucker <john@larkintuckerllc.com>
2017-08-01 10:20:09 -0400

    above and below

--- page-methodology.php
+++ page-methodology.php
@@ -15,7 +15,7 @@
     wp_register_script('fpi_metrics_main',
       get_template_directory_uri() .
       '/metrics/dist/main.bundle.js',
-      array('fpi_metrics_vendor'), '2017020101', true);
+      array('fpi_metrics_vendor'), '2017080101', true);
     wp_enqueue_style('fpi_metrics_style');
     wp_enqueue_script('fpi_metrics_manifest');
     wp_enqueue_script('fpi_metrics_vendor');
@@ -24,8 +24,12 @@
   add_action('wp_enqueue_scripts', 'fpi_metrics_add_scripts');
 ?>
 <?php get_header(); ?>
+<?php while (have_posts()) : the_post(); ?>
 <div class="container">
   <h3>Methodology</h3>
+  <?php the_content(); ?>
   <div id="root"></div>
+  <?php the_field('below_metrics'); ?>
 </div>
+<?php endwhile; ?>
 <?php get_footer(); ?>
