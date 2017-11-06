3cd6b88b7b7fd0115ccb19181db4561d3636feb4
John  Tucker <john@larkintuckerllc.com>
2017-11-06 09:36:53 -0500

    Rebrand

--- front-page.php
+++ front-page.php
@@ -1,14 +1,36 @@
-<!DOCTYPE html>
-<html lang="en" id="dev__html">
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-    <title>FPI Lab</title>
-    <?php wp_head() ?>
-  </head>
-  <body id="dev__body" class="body--embed">
-    <div id="dev__root">
-      <h2 id="dev__root__message">[ Under Development ]</h2>
+<?php get_header(); ?>
+<div
+  class="jumbotron"
+  style="background-image: url(<?php echo get_template_directory_uri() . '/img/hero.jpg'; ?>);"
+>
+  <div id="home_body__hero" class="container">
+    <div id="home_body__hero__image" class="visible-md-block visible-lg-block">
+      <img src="<?php echo get_template_directory_uri() . '/img/james_anderson.jpg'; ?>" />
     </div>
-  <?php wp_footer() ?>
-  </body>
-  </html>
+    <div>
+      <h1>James L. Anderson</h1>
+      <p>Director, Institute for Sustainable Food Systems, University of Florida<br />
+      Professor, Food and Resource Economics, University of Florida</p>
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div id="home_body__news" class="row">
+    <?php $temp_query = $wp_query; ?>
+    <?php query_posts( 'category_name=News&posts_per_page=4' ); ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+      <div class="home_body__news__item col-sm-3">
+        <div
+          <?php if ( has_post_thumbnail() ): ?>
+          style="background-image: url('<?php the_post_thumbnail_url('large'); ?>');"
+          <?php endif ?>
+          class="home_body__news__item__image"
+          onclick="window.location.assign('<?php the_permalink(); ?>')"
+        ></div>
+        <?php the_excerpt(); ?>
+      </div>
+    <?php endwhile; ?>
+    <?php $wp_query = $temp_query; ?>
+  </div>
+</div>
+<?php get_footer(); ?>
