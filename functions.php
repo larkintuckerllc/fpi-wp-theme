68550e9fa48aa3c1c903f4a0e967cf99cb8f68f9
John Tucker <john@larkintuckerllc.com>
2020-08-28 21:06:14 -0400

    api post type

--- functions.php
+++ functions.php
@@ -118,7 +118,7 @@
       )
     );
   }
-  function fpi_create_post_type_fpi() {
+  function fpi_create_post_type_indicator() {
     $labels = array(
      'name' => __( 'FPI Indicators', 'fpi' ),
      'singular_name' => __( 'FPI Indicator', 'fpi' ),
@@ -342,6 +342,230 @@
     	));
     }
   }
+  function fpi_create_post_type_api() {
+    $labels = array(
+     'name' => __( 'API Indicators', 'fpi' ),
+     'singular_name' => __( 'API Indicator', 'fpi' ),
+     'add_new' => __( 'Add New' , 'fpi' ),
+     'add_new_item' => __( 'Add New API Indicator' , 'fpi' ),
+     'edit_item' =>  __( 'Edit API Indicator' , 'fpi' ),
+     'new_item' => __( 'New API Indicator' , 'fpi' ),
+     'view_item' => __('View API Indicator', 'fpi'),
+     'search_items' => __('Search API Indicators', 'fpi'),
+     'not_found' =>  __('No API Indicators found', 'fpi'),
+     'not_found_in_trash' => __('No API Indicators found in Trash', 'fpi'),
+    );
+    register_post_type( 'fpi_api',
+      array(
+        'labels' => $labels,
+        'public' => true,
+        'has_archive' => false,
+        'menu_icon' => 'dashicons-list-view',
+      )
+    );
+    if( function_exists( 'register_field_group' ) ) {
+    	register_field_group(array (
+    		'id' => 'acf_fpi_api',
+    		'key' => 'acf_fpi_api',
+    		'title' => 'API Indicator',
+    		'fields' => array (
+          array (
+    				'key' => 'field_fpi_api_import_id',
+    				'label' => 'Import Id',
+    				'name' => 'import_id',
+    				'type' => 'number',
+    				'instructions' => 'The unique import id; used for matching.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'min' => '',
+    				'max' => '',
+    				'step' => '',
+    			),
+    			array (
+    				'key' => 'field_fpi_api_name',
+    				'label' => 'Name',
+    				'name' => 'name',
+    				'type' => 'text',
+    				'instructions' => 'User-friendly name of the fishery.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'formatting' => 'none',
+    				'maxlength' => '',
+    			),
+    			array (
+    				'key' => 'field_fpi_api_species',
+    				'label' => 'Species',
+    				'name' => 'species',
+    				'type' => 'text',
+    				'instructions' => 'Related species.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'formatting' => 'none',
+    				'maxlength' => '',
+    			),
+    			array (
+    				'key' => 'field_fpi_api_image',
+    				'label' => 'Image',
+    				'name' => 'image',
+    				'type' => 'image',
+    				'instructions' => '300px by 200px image representing fishery.',
+    				'required' => 1,
+    				'save_format' => 'url',
+    				'preview_size' => 'thumbnail',
+    				'library' => 'all',
+    			),
+          array (
+    				'key' => 'field_fpi_api_year',
+    				'label' => 'Year',
+    				'name' => 'year',
+    				'type' => 'number',
+    				'instructions' => 'Year.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'min' => '',
+    				'max' => '',
+    				'step' => '',
+    			),
+    			array (
+    				'key' => 'field_fpi_api_latitide',
+    				'label' => 'Latitude',
+    				'name' => 'latitude',
+    				'type' => 'number',
+    				'instructions' => 'Latitude (+/- 90) representing fishery.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'min' => '-90',
+    				'max' => 90,
+    				'step' => '',
+    			),
+    			array (
+    				'key' => 'field_fpi_api_longitude',
+    				'label' => 'Longitude',
+    				'name' => 'longitude',
+    				'type' => 'number',
+    				'instructions' => 'Longitude (+/- 180) representing fishery.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'min' => '-180',
+    				'max' => 180,
+    				'step' => '',
+    			),
+    			array (
+    				'key' => 'field_fpi_api_ecological',
+    				'label' => 'Ecological',
+    				'name' => 'ecological',
+    				'type' => 'number',
+    				'instructions' => 'Ecological indicator (0-5) of the fishery.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'min' => 0,
+    				'max' => 5,
+    				'step' => '',
+    			),
+    			array (
+    				'key' => 'field_fpi_api_economic',
+    				'label' => 'Economic',
+    				'name' => 'economic',
+    				'type' => 'number',
+    				'instructions' => 'Economic indicator (0-5) of the fishery.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'min' => 0,
+    				'max' => 5,
+    				'step' => '',
+    			),
+    			array (
+    				'key' => 'field_fpi_api_community',
+    				'label' => 'Community',
+    				'name' => 'community',
+    				'type' => 'number',
+    				'instructions' => 'Community indicator (0-5) of the fishery.',
+    				'required' => 1,
+    				'default_value' => '',
+    				'placeholder' => '',
+    				'prepend' => '',
+    				'append' => '',
+    				'min' => 0,
+    				'max' => 5,
+    				'step' => '',
+    			),
+          array (
+    				'key' => 'field_fpi_api_description',
+    				'label' => 'Description',
+    				'name' => 'description',
+    				'type' => 'wysiwyg',
+    				'default_value' => '',
+    				'toolbar' => 'full',
+    				'media_upload' => 'no',
+    			),
+          array (
+            'key' => 'field_fpi_api_custom',
+            'label' => 'Custom',
+            'name' => 'custom',
+            'type' => 'wysiwyg',
+            'default_value' => '',
+            'toolbar' => 'full',
+            'media_upload' => 'yes',
+          ),
+    		),
+    		'location' => array (
+    			array (
+    				array (
+    					'param' => 'post_type',
+    					'operator' => '==',
+    					'value' => 'fpi_api',
+    					'order_no' => 0,
+    					'group_no' => 0,
+    				),
+    			),
+    		),
+    		'options' => array (
+    			'position' => 'normal',
+    			'layout' => 'no_box',
+    			'hide_on_screen' => array (
+    				0 => 'permalink',
+    				1 => 'the_content',
+    				2 => 'excerpt',
+    				3 => 'discussion',
+    				4 => 'comments',
+    				5 => 'revisions',
+    				6 => 'slug',
+    				7 => 'author',
+    				8 => 'format',
+    				9 => 'featured_image',
+    				10 => 'categories',
+    				11 => 'tags',
+    				12 => 'send-trackbacks',
+    			),
+    		),
+    		'menu_order' => 0,
+    	));
+    }
+  }
   function fpi_single_template( $single ) {
 		global $post;
 		if ($post->post_type == 'fpi_indicator'){
@@ -403,7 +627,8 @@
   add_action( 'init', 'fpi_create_post_type_data_custom' );
   add_action( 'init', 'fpi_create_post_type_menus' );
   add_action('wp_enqueue_scripts', 'fpi_add_theme_scripts');
-  add_action( 'init', 'fpi_create_post_type_fpi' );
+  add_action( 'init', 'fpi_create_post_type_indicator' );
+  add_action( 'init', 'fpi_create_post_type_api' );
   add_action( 'init', 'fpi_register_my_menus' );
   add_action( 'single_template', 'fpi_single_template');
   add_action( 'init', 'fpi_add_rewrite_rules' );
